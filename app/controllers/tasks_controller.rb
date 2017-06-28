767911b1d6c7bfe74d12a5c96827bef4e42c0527
Anchieta Junior <santosjr87@gmail.com>
2017-06-28 12:03:42 -0300

    Added verifications

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,7 +2,7 @@ class TasksController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @tasks = Task.where(deadline: Date.today).order(:importance)
+    @tasks = Task.where(user_id: current_user.id).where(deadline: Date.today).order(:importance)
     @task = Task.new
   end
 
